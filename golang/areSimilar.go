eb7d81d5389a4182a930d305f6e01f816e8f317c
Vagner do Carmo <vluzrmos@gmail.com>
2021-12-17 01:54:06 -0300

    Melhora areSimiliar.go. Ainda não passa dos 19/20 tests.

--- golang/areSimilar.go
+++ golang/areSimilar.go
@@ -13,7 +13,7 @@ func solution(a []int, b []int) bool {
 
 		found := false
 
-		for t := i + 1; t < lenOfB; t++ {
+		for t := i + 1; swaps == 0 && t < lenOfB; t++ {
 			if a[i] == b[t] {
 				b[i], b[t] = b[t], b[i]
 				found = true
@@ -22,16 +22,16 @@ func solution(a []int, b []int) bool {
 			}
 		}
 
-		if !found || swaps > 1 {
+		if !found {
 			return false
 		}
 	}
 
-	return true
+	return swaps <= 1
 }
 
 func main() {
-	input := [][]int{{9, 8, 3, 4, 5, 6, 7, 2, 1}, {1, 2, 3, 4, 5, 6, 7, 8, 9}}
+	input := [][]int{{1, 2, 3, 4, 5, 6, 7, 9, 8}, {1, 2, 3, 4, 5, 6, 7, 8, 9}}
 
 	fmt.Println("INPUT:", input)
 	fmt.Println("OUTPUT:", solution(input[0], input[1]))
