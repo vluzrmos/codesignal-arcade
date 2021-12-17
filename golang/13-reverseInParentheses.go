3ea6a09a13a1c3f3964d18c0c2634c4dc7715f69
Vagner do Carmo <vluzrmos@gmail.com>
2021-12-17 02:35:16 -0300

    Add readme, fix questions number

--- golang/13-reverseInParentheses.go
+++ golang/13-reverseInParentheses.go
@@ -5,17 +5,6 @@ import (
 	"strings"
 )
 
-/**
-For inputString = "(bar)", the output should be
-solution(inputString) = "rab";
-For inputString = "foo(bar)baz", the output should be
-solution(inputString) = "foorabbaz";
-For inputString = "foo(bar)baz(blim)", the output should be
-solution(inputString) = "foorabbazmilb";
-For inputString = "foo(bar(baz))blim", the output should be
-solution(inputString) = "foobazrabblim".
-Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
-*/
 func StringReverse(input string) string {
 	if len(input) <= 1 {
 		return input
---
+++ golang/readme.md
@@ -0,0 +1,6 @@
+
+### To run an exercise
+
+```bash
+go run filename.go
+```
